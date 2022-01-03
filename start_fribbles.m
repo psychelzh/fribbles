c44cebee7bd22666568d0259df49cf2cdb702462
Liang Zhang <psychelzh@outlook.com>
2022-01-03 16:43:51 +0800

    add trigger
    fix #1
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- start_fribbles.m
+++ start_fribbles.m
@@ -6,14 +6,25 @@ arguments
     args.Part {mustBeMember(args.Part, ["prac", "test"])} = "prac"
     args.Treat {mustBeMember(args.Treat, ["exp", "ctrl"])} = "exp"
     args.Phase {mustBeMember(args.Phase, ["encoding", "retrieval"])} = "encoding"
+    args.SendTrigger {mustBeNumericOrLogical}
 end
 part = args.Part;
 treat = args.Treat;
 phase = args.Phase;
+if ismember('SendTrigger', fieldnames(args))
+    send_trigger = args.SendTrigger;
+else
+    send_trigger = part == "test";
+end
+
+% --- prepare for trigger ---
+if send_trigger
+    ioObj = io64;
+    address = hex2dec('C100');
+end
 
 % ---- prepare sequences ----
-args_cell = namedargs2cell(args);
-config = init_config(args_cell{:});
+config = init_config();
 
 % ---- set default error related outputs ----
 status = 0;
@@ -22,6 +33,8 @@ exception = [];
 % ---- set experiment timing parameters (predefined here, all in secs) ----
 % fixation duration
 time_fixation_secs = 1;
+% random blank interval range
+time_blank_interval = [0.2, 0.4];
 % stimuli duration
 time_stimuli_secs_min = 0.5;
 if treat == "exp"
@@ -128,8 +141,17 @@ try
             break
         end
     end
+    last_block_id = 0;
     for i_trial = 1:height(config)
         if early_exit, break, end
+        cur_trial = config(i_trial, :);
+        if cur_trial.block_id ~= last_block_id
+            if send_trigger
+                % send trigger: each new block start
+                io64(ioObj, address, 30 + cur_trial.block_id);
+            end
+            last_block_id = cur_trial.block_id;
+        end
         % trial part 1: fixation
         num_frames = round(time_fixation_secs / ifi);
         vbl = Screen('Flip', window_ptr);
@@ -144,12 +166,34 @@ try
             vbl = Screen('Flip', window_ptr, vbl + 0.5 * ifi);
         end
         if early_exit, break, end
+        % random blank interval
+        time_rand_secs = time_blank_interval(1) + ...
+            (time_blank_interval(2) - time_blank_interval(1)) * rand(1, 1);
+        num_frames = round(time_rand_secs / ifi);
+        vbl = Screen('Flip', window_ptr);
+        for i = 1:num_frames
+            [~, ~, resp_code] = KbCheck(-1);
+            if resp_code(keys('exit'))
+                early_exit = true;
+                break
+            end
+            vbl = Screen('Flip', window_ptr, vbl + 0.5 * ifi);
+        end
+        if early_exit, break, end
         % trial part 2: stimuli
-        cur_trial = config(i_trial, :);
         num_frames = round(time_stimuli_secs_max / ifi);
         vbl = Screen('Flip', window_ptr);
         if treat == "exp" && phase == "encoding"
             stim_onset_timestamp = vbl;
+            if send_trigger
+                % send trigger: stimuli onset
+                io64(ioObj, address, 11);
+            end
+        elseif treat == "exp" && phase == "retrieval"
+            if send_trigger
+                % send trigger: stimuli onset
+                io64(ioObj, address, 21);
+            end
         end
         for i = 1:num_frames
             draw_stimuli(cur_trial, 'hide_crown')
@@ -178,11 +222,52 @@ try
         if early_exit, break, end
         if treat == "exp" && phase == "encoding"
             [resp, resp_raw, resp_time] = analyze_response();
+            if send_trigger
+                if resp == ""
+                    code = 3;
+                else
+                    code = resp ~= cur_trial.cresp + 1;
+                end
+                % send trigger: stimuli offset or response made (encoding)
+                io64(ioObj, address, 12);
+                % send trigger: response code
+                io64(ioObj, address, code);
+            end
         end
+        % random blank interval
+        time_rand_secs = time_blank_interval(1) + ...
+            (time_blank_interval(2) - time_blank_interval(1)) * rand(1, 1);
+        num_frames = round(time_rand_secs / ifi);
+        vbl = Screen('Flip', window_ptr);
+        for i = 1:num_frames
+            [~, ~, resp_code] = KbCheck(-1);
+            if resp_code(keys('exit'))
+                early_exit = true;
+                break
+            end
+            vbl = Screen('Flip', window_ptr, vbl + 0.5 * ifi);
+        end
+        if early_exit, break, end
         % trial part 3: post stimuli phase
         num_frames = round(time_poststim_secs / ifi);
         vbl = Screen('Flip', window_ptr);
-        if ~(treat == "exp" && phase == "encoding")
+        if treat == "exp" && phase == "encoding"
+            if send_trigger
+                % send trigger: crown onset
+                io64(ioObj, address, 13);
+            end
+        else
+            if treat == "exp" && phase == "retrieval"
+                if send_trigger
+                    if cur_trial.cresp == "correct"
+                        code = 22;
+                    else
+                        code = 23;
+                    end
+                    % send trigger: crown onset with type
+                    io64(ioObj, address, code);
+                end
+            end
             stim_onset_timestamp = vbl;
         end
         for i = 1:num_frames
@@ -225,8 +310,26 @@ try
             vbl = Screen('Flip', window_ptr, vbl + 0.5 * ifi);
         end
         if early_exit, break, end
-        if ~(treat == "exp" && phase == "encoding")
+        if treat == "exp" && phase == "encoding"
+            if send_trigger
+                % send trigger: crown offset
+                io64(ioObj, address, 14);
+            end
+        else
             [resp, resp_raw, resp_time] = analyze_response();
+            if treat == "exp" && phase == "retrieval"
+                if send_trigger
+                    if resp == ""
+                        code = 3;
+                    else
+                        code = resp ~= cur_trial.cresp + 1;
+                    end
+                    % send trigger: stimuli offset or response made (retrieval)
+                    io64(ioObj, address, 24);
+                    % send trigger: response code
+                    io64(ioObj, address, code);
+                end
+            end
         end
         % record user's response
         recordings.resp(i_trial) = resp;
@@ -271,6 +374,94 @@ ShowCursor;
 Screen('Preference', 'VisualDebugLevel', old_visdb);
 Screen('Preference', 'SkipSyncTests', old_sync);
 Priority(old_pri);
+
+    function config = init_config()
+        switch part
+            case "prac"
+                if treat == "exp"
+                    % set different random seed when practice
+                    rng('Shuffle')
+                else
+                    % use the same random seed for control
+                    rng(sum(char(treat)))
+                end
+                num_blocks = 1;
+                num_trials = 6;
+            case "test"
+                % same random seed for entire control treatment
+                if treat == "exp"
+                    rng(sum(char(treat + phase)))
+                else
+                    rng(sum(char(treat)))
+                end
+                num_blocks = 5;
+                num_trials = 60;
+        end
+        config_stim = readtable(fullfile('config', 'stimuli.csv'));
+        [stim_id_left, stim_id_right] = meshgrid(config_stim.stim_id, config_stim.stim_id);
+        stim_pairs = table(stim_id_left(:), stim_id_right(:), ...
+            'VariableNames', {'stim_id_left', 'stim_id_right'});
+        [~, row_id_left] = ismember(stim_pairs.stim_id_left, config_stim.stim_id);
+        [~, row_id_right] = ismember(stim_pairs.stim_id_right, config_stim.stim_id);
+        stim_pairs.speed_left = config_stim.speed(row_id_left);
+        stim_pairs.speed_right = config_stim.speed(row_id_right);
+        stim_pairs = stim_pairs(stim_pairs.speed_left ~= stim_pairs.speed_right, :);
+        % generate sequence for each block
+        config = table();
+        config.block_id = repelem(1:num_blocks, num_trials / num_blocks)';
+        config.trial_id = (1:num_trials)';
+        stims = datasample(stim_pairs, num_trials, 'Replace', false);
+        sides = ["left", "right"]';
+        stims.win_side = sides((stims.speed_left < stims.speed_right) + 1);
+        switch treat
+            case "exp"
+                if phase == "encoding"
+                    stims.crown_side = sides((stims.speed_left < stims.speed_right) + 1);
+                    stims.cresp = stims.win_side;
+                else
+                    stims.crown_side = datasample(sides, height(stims));
+                    resps = ["incorrect", "correct"]';
+                    stims.cresp = resps((stims.win_side == stims.crown_side) + 1);
+                end
+            case "ctrl"
+                if part == "prac"
+                    num_ctrl = 2;
+                else
+                    num_ctrl = 24;
+                end
+                % prepare encoding part
+                is_ctrl_enc = false(num_trials, 1);
+                row_ctrl_enc = datasample(1:num_trials, num_ctrl, 'Replace', false);
+                is_ctrl_enc(row_ctrl_enc) = true;
+                stims.is_ctrl_enc = is_ctrl_enc;
+                texts = ["慢于", "快于"]';
+                stims.text_tip = texts(xor(stims.win_side == "left", is_ctrl_enc) + 1);
+                % prepare retrieval part
+                if phase == "retrieval"
+                    is_ctrl_ret = false(num_trials, 1);
+                    row_ctrl_ret = [datasample( ...
+                        setdiff(1:num_trials, row_ctrl_enc), num_ctrl / 2, ...
+                        'Replace', false), ...
+                        datasample(row_ctrl_enc, num_ctrl / 2, 'Replace', false)];
+                    is_ctrl_ret(row_ctrl_ret) = true;
+                    stims.is_ctrl_ret = is_ctrl_ret;
+                    stims = datasample(stims, num_trials);
+                    stims.cresp = sides((stims.is_ctrl_enc ~= stims.is_ctrl_ret) + 1);
+                else
+                    stims.cresp = strings(num_trials, 1);
+                    for i_row = 1:num_trials
+                        if stims.is_ctrl_enc(i_row)
+                            stims.cresp(i_row) = "control";
+                        else
+                            stims.cresp(i_row) = stims.win_side(i_row);
+                        end
+                    end
+                end
+        end
+        config = horzcat(config, stims);
+        rng('default')
+    end
+
 
     function draw_stimuli(config, feedback)
         if treat == "exp"
